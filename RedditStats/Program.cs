25b61046858f3baca3769e2cdd17ef42ff983d98
Ripal Barot <ripplejb@hotmail.com>
2023-09-11 22:55:30 -0500

    Fixed thread.sleep issue

--- RedditStats/Program.cs
+++ RedditStats/Program.cs
@@ -9,16 +9,8 @@ var cancellationTokenSource = new CancellationTokenSource();
 
 var token = cancellationTokenSource.Token;
 
-app.Run(token);
+await app.Run(token);
 
-while (!File.Exists("cancel.txt"))
-{
-    Thread.Sleep(5000);
-}
-Console.WriteLine("User cancelled...");
-cancellationTokenSource.Cancel();
-cancellationTokenSource.Dispose();
-Console.WriteLine("Shutting Down...");
-Thread.Sleep(10000);
+
 
 
